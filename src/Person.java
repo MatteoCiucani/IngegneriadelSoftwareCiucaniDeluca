4acdb049650e6b548a015f0b3de1206769633256
MarcoDeLuca21 <marco.deluca4@stud.unifi.if>
2023-10-09 12:06:14 +0200

    correzione classe Person

--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,17 @@
 public class Person {
-    public Person(String name, String address, String email, int phone, AccountType accountType) {
+    public Person(String name, String address, String email, int phone, String gender, AccountType accountType) {
         this.name = name;
         this.address = address;
         this.email = email;
         this.phone = phone;
+        this.gender = gender;
         this.accountType = accountType;
     }
     private String name;
     private String address;
     private String email;
     private int phone;
+    private String gender;
     private AccountType accountType;
 
 
