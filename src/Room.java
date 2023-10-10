9c40f108e1bb2121c39b21c5db7f026705360ea4
MarcoDeLuca21 <marco.deluca4@stud.unifi.if>
2023-10-10 17:06:20 +0200

    implementazione classe Room

--- src/Room.java
+++ src/Room.java
@@ -1,4 +1,9 @@
 public class Room {
+    private int roomNumber;
+    private RoomStyle roomStyle;
+    private RoomStatus roomStatus;
+    private double bookingPrice;
+
     public Room(int roomNumber, RoomStyle roomStyle, RoomStatus roomStatus, double bookingPrice) {
         this.roomNumber = roomNumber;
         this.roomStyle = roomStyle;
@@ -6,8 +11,34 @@ public class Room {
         this.bookingPrice = bookingPrice;
     }
 
-    private int roomNumber;
-    private RoomStyle roomStyle;
-    private RoomStatus roomStatus;
-    private double bookingPrice;
+    public int getRoomNumber(){
+        return roomNumber;
+    }
+
+    public void setRoomNumber(){
+        this.roomNumber = roomNumber;
+    }
+
+    public RoomStyle getRoomStyle(){
+        return roomStyle;
+    }
+
+    public void setRoomStyle() {
+        this.roomStyle = roomStyle;
+    }
+
+    public RoomStatus getRoomStatus(){
+        return roomStatus;
+    }
+    public void setRoomStatus(){
+        this.roomStatus = roomStatus;
+    }
+
+    public double getBookingPrice(){
+        return bookingPrice;
+    }
+
+    public void setBookingPrice(double bookingPrice) {
+        this.bookingPrice = bookingPrice;
+    }
 }
