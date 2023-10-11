bab828fc0800f2f2e080393cbb2662e268913f26
Matteo Ciucani <matteo.ciucani@stud.unifi.it>
2023-10-11 15:23:42 +0200

    aggiunta metodo SearchRoom

--- src/Receptionist.java
+++ src/Receptionist.java
@@ -1,5 +1,27 @@
-public class Receptionist extends Person{
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Receptionist extends Person implements Search{
+    private List<Room> rooms;
     public Receptionist(String name, String address, String email, int phone, String gender, AccountType accountType) {
         super(name, address, email, phone, gender, accountType);
     }
+
+    @Override
+    public List<Room> searchRoom(RoomStyle roomStyle, LocalDateTime startdate, int duration) {
+        List<Room> availableRooms = new ArrayList<>();
+        for (Room room : rooms) {
+            // Verifica se la camera è disponibile in base allo stile e alla data
+            if (room.getRoomStyle() == roomStyle /*&& isRoomAvailable(room, startdate, duration)*/) {
+                availableRooms.add(room);
+            }
+        }
+
+        return availableRooms;
+    }
+
+
+
+
 }
--- src/Search.java
+++ src/Search.java
@@ -1,2 +1,5 @@
+import java.time.LocalDateTime;
+import java.util.List;
 public interface Search {
+    List<Room> searchRoom(RoomStyle roomStyle, LocalDateTime startdate, int duration);
 }
