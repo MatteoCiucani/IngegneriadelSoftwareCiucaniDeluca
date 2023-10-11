c71c0a9e43bfd7a829a8bb0bca6dadf9bc72f698
Matteo Ciucani <matteo.ciucani@stud.unifi.it>
2023-10-11 14:39:16 +0200

    correzione classe HotelManager

--- src/HotelManager.java
+++ src/HotelManager.java
@@ -1,5 +1,5 @@
 public class HotelManager extends Person{
-    public HotelManager(String name, String address, String email, int phone, AccountType accountType) {
-        super(name, address, email, phone, accountType);
+    public HotelManager(String name, String address, String email, int phone, String gender, AccountType accountType) {
+        super(name, address, email, phone, gender, accountType);
     }
 }
